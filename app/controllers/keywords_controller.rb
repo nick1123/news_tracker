a32b1dc6731f4336547d4ecb6ff585fae0e1721b
Nick Stewart <nicholas4@gmail.com>
2014-10-25 14:33:35 -0600

    Fix 24hr issue

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,8 +4,7 @@ class KeywordsController < ApplicationController
     ordering = "occurences DESC, phrase ASC"
     min_occurences = 2
     @week = {}
-    @week["Last 24 Hours"] = Keyword.where(created_at: (24.hours.ago..Time.now)).where("occurences >= ?", min_occurences).order(ordering)
-    (1..6).each do |days_back|
+    (0..6).each do |days_back|
       day = Date.current - days_back
       day_display = day.strftime("%A %B %-d, %Y")
       keywords = Keyword.where(on_date: day).where("occurences >= ?", min_occurences).order(ordering)
